20694bae7656eb79cc773e3fe24e86736b4e15f3
vincenttian <vincenttian16@gmail.com>
2014-05-30 22:30:05 -0700

    working tutorial

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+var express = require('express'),
+    FB = require('fb'),
+    http = require('http'),
+    path = require('path'),
 
-var express       = require('express'),
-    FB            = require('../../fb'),
-    http          = require('http'),
-    path          = require('path'),
+    config = require('./config'),
 
-    config        = require('./config'),
-
-    api           = require('./routes/api'),
-    home          = require('./routes/home'),
-    meals         = require('./routes/meals');
+    api = require('./routes/api'),
+    home = require('./routes/home'),
+    meals = require('./routes/meals');
 
 var app = express();
 
-if(!config.facebook.appId || !config.facebook.appSecret) {
+if (!config.facebook.appId || !config.facebook.appSecret) {
     throw new Error('facebook appId and appSecret required in config.js');
 }
 
@@ -23,7 +22,9 @@ app.configure(function() {
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.cookieParser());
-    app.use(express.cookieSession({ secret: 'secret'}));
+    app.use(express.cookieSession({
+        secret: 'secret'
+    }));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
@@ -34,14 +35,14 @@ app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
-app.get( '/',                home.index);
-app.get( '/login/callback',  home.loginCallback);
-app.get( '/logout',          home.logout);
-app.get( '/search',          api.search);
-app.get( '/friends',         api.friends);
-app.post('/announce',        api.announce);
-app.get( '/meals/:id',       meals.show);
-app.get( '/winjs/meals/:id', meals.showWinJs); // this is used for facebook-winjs-sdk sample and not required to actually run this sample
+app.get('/', home.index);
+app.get('/login/callback', home.loginCallback);
+app.get('/logout', home.logout);
+app.get('/search', api.search);
+app.get('/friends', api.friends);
+app.post('/announce', api.announce);
+app.get('/meals/:id', meals.show);
+app.get('/winjs/meals/:id', meals.showWinJs); // this is used for facebook-winjs-sdk sample and not required to actually run this sample
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
