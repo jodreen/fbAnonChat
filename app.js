79019ec66929f7bf6afba5aff1e1c1660f0adf5a
vincenttian <vincenttian16@gmail.com>
2014-06-05 20:37:59 -0700

    added fb api stuff for friend validation

--- app.js
+++ app.js
@@ -38,18 +38,67 @@ app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
+available_people = [];
+
 app.get('/', home.index);
 app.get('/login/callback', home.loginCallback);
 app.get('/logout', home.logout);
-app.get('/search', api.search);
-app.get('/friends', api.friends);
-app.post('/announce', api.announce);
+// app.get('/friends', api.friends);
 app.get('/chat/:id', function(req, res, next) {
-    if (true) { // logic for shit
-        res.render('chat.ejs');
-    } else {
-        next();
-    }
+    FB.api('fql', {
+        q: 'SELECT uid2 FROM friend WHERE uid1 = me()',
+        access_token: req.session.access_token
+    }, function(re) {
+        if (!re || re.error) {
+            console.log(!re ? 'error occurred' : re.error);
+            return;
+        }
+        FB.api('fql', {
+            q: 'SELECT uid FROM user WHERE uid=me()',
+            access_token: req.session.access_token
+        }, function(r) {
+            if (!re || re.error) {
+                console.log(!re ? 'error occurred' : re.error);
+                return;
+            }
+            var friends_list = re.data;
+            var num_friends = friends_list.length;
+            var mid = Math.floor(friends_list.length / 2);
+
+            // Iterate through all people that are available to chat
+            for (var i = 0; i < available_people.length; i++) {
+                // Binary Search
+                var minIndex = 0;
+                var maxIndex = friends_list.length - 1;
+                var currentIndex;
+                var currentElement;
+                while (minIndex <= maxIndex) {
+                    currentIndex = (minIndex + maxIndex) / 2 | 0;
+                    currentElement = friends_list[currentIndex];
+                    if (currentElement < available_people[i]) {
+                        minIndex = currentIndex + 1;
+                    } else if (currentElement > available_people[i]) {
+                        maxIndex = currentIndex - 1;
+                    } else {
+                        // Found a match at currentIndex!
+                        console.log('found a match!');
+                        available_people.splice(currentIndex, 1);
+                        break;
+                    }
+                }
+                // No Matches   
+                console.log('no matches!');
+                available_people.push(r.data[0]['uid']);
+            }
+            available_people.push(r.data[0]['uid']);
+            console.log(r.data[0]['uid']);
+            if (true) { // logic for shit
+                res.render('chat.ejs');
+            } else {
+                next();
+            }
+        });
+    });
 })
 
 // Chatroom
