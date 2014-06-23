21311c8455bb6864bf4322b3b9a1bd9e931683e1
vincenttian <vincenttian16@gmail.com>
2014-06-22 18:31:39 -0700

    last try

--- app.js
+++ app.js
@@ -282,7 +282,8 @@ app.get('/', function(req, res) {
 
                 res.json({
                     friendlist: friends_list_2,
-                    friendlist1: friends_list
+                    friendlist1: friends_list,
+                    anything: re.data
                 });
                 return;
 
